8e31df6ffb5f562bdc4c3e685f14978d3d72d524
2bt <s8572327@gmail.com>
2021-10-23 18:11:13 +0200

    improve speed table length estimation

--- src/main.cpp
+++ src/main.cpp
@@ -257,10 +257,10 @@ bool Sid2Song::run() {
                     // inc tempo
                     if (cmd == 0xf && arg >=2) ++arg;
 
-                    if (cmd >= 0x1 and cmd <= 0x4) {
-                        max_table[3] = std::max(max_table[3], arg);
+                    if (cmd >= 0x1 && cmd <= 0x4 || cmd == 0xe) {
+                        max_table[gt::STBL] = std::max(max_table[gt::STBL], arg);
                     }
-                    if (cmd >= 0x8 and cmd <= 0xa) {
+                    if (cmd >= 0x8 && cmd <= 0xa) {
                         max_table[cmd - 0x8] = std::max(max_table[cmd - 0x8], arg);
                     }
                 }
@@ -361,6 +361,10 @@ bool Sid2Song::run() {
             }
         }
         if (t == gt::STBL) {
+            while (peek() != 0) {
+                m_song.ltable[t][max_table[t]] = read();
+                ++max_table[t];
+            }
             int x = read();
             if (x != 0) {
                 printf("ERROR: speed table\n");
@@ -387,6 +391,12 @@ bool Sid2Song::run() {
                 int x = m_song.ltable[t][i];
                 if (x > 0x80 && x < 0xff) {
                     m_song.ltable[t][i] = (x << 1) | 0x80;
+                }
+            }
+            if (t == gt::STBL) {
+                uint8_t x = m_song.ltable[t][i];
+                if (x >= 0xf1 && x <= 0xf4 || x == 0xfe) {
+                    max_table[gt::STBL] = std::max<int>(max_table[gt::STBL], m_song.rtable[t][i]);
                 }
             }
 
